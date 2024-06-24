eca239c5ef9f9614b3d8193b630266764e6a4ebf
hi100e <hi@workoak.com>
2024-06-24 14:08:43 +0400

    reorg omap in its own pkg

---
+++ omap/map_test.go
@@ -0,0 +1,75 @@
+package omap_test
+
+import (
+	"reflect"
+	"testing"
+
+	omap "github.com/workoak/wogo/omap"
+)
+
+func TestIterateByKey(t *testing.T) {
+	type OData omap.OMap[string, string]
+	testMap := OData{
+		"a": "aVal",
+		"x": "xVal",
+		"c": "cVal",
+		"b": "bVal",
+	}
+	want := []string{"aVal", "bVal", "cVal", "xVal"}
+	var got []string
+	for iter := omap.IteratorByKey(testMap); iter.HasNext(); {
+		_, v := iter.Next()
+		got = append(got, v)
+	}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v,wanted %v ", got, want)
+	}
+
+}
+
+func TestIterateByValue(t *testing.T) {
+
+	type ValStruct struct {
+		Fnum  int
+		FName string
+	}
+	type OData map[string]*ValStruct
+	testMap := OData{
+		"a": &ValStruct{Fnum: 4, FName: "aStruct"},
+		"x": &ValStruct{Fnum: 2, FName: "xStruct"},
+		"c": &ValStruct{Fnum: 3, FName: "cStruct"},
+		"b": &ValStruct{Fnum: 1, FName: "bStruct"},
+	}
+	t.Run("iterate by fnum order", func(t *testing.T) {
+		//order by Fnum
+		want := []*ValStruct{testMap["b"], testMap["x"], testMap["c"], testMap["a"]}
+		var got []*ValStruct
+		lessFunc := func(i, j *ValStruct) bool {
+			return i.Fnum < j.Fnum
+		}
+		for iter := omap.IterateByValue(testMap, lessFunc); iter.HasNext(); {
+			_, v := iter.Next()
+			got = append(got, v)
+		}
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v,wanted %v ", got, want)
+		}
+	})
+
+	t.Run("iterate by fname order", func(t *testing.T) {
+		//order by Fnum
+		want := []*ValStruct{testMap["a"], testMap["b"], testMap["c"], testMap["x"]}
+		var got []*ValStruct
+		lessNameFunc := func(i, j *ValStruct) bool {
+			return i.FName < j.FName
+		}
+		for iter := omap.IterateByValue(testMap, lessNameFunc); iter.HasNext(); {
+			_, v := iter.Next()
+			got = append(got, v)
+		}
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v,wanted %v ", got, want)
+		}
+	})
+
+}

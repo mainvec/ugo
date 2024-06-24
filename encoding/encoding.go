e46f8ad6bd3075d1b19a31de8d835ede2573ea06
hi100e <hi@workoak.com>
2024-06-24 14:09:54 +0400

    encoding interface and registry

---
+++ encoding/encoding.go
@@ -0,0 +1,27 @@
+package util
+
+import "github.com/workoak/wogo/registry"
+
+var (
+	encodingsRegistry = registry.NewRegistry[Encoding]()
+)
+
+// Encoding is an interface that defines the methods that an encoding type must implement.
+// No encoding type is provided by this package.
+type Encoding interface {
+	Encode(any) ([]byte, error)
+	Decode([]byte, any) error
+	MimeType() string
+}
+
+func RegisterEncoding(name string, enc Encoding) {
+	encodingsRegistry.Register(name, enc)
+}
+
+func LookupEncoding(name string) (Encoding, bool) {
+	return encodingsRegistry.Lookup(name)
+}
+
+func ListEncodings() []string {
+	return encodingsRegistry.List()
+}

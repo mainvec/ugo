0ebfd4725e2a1b0dcf7ff7912b46486710d9b303
hi100e <hi@workoak.com>
2024-06-24 14:08:09 +0400

    a simple registry implementation

---
+++ registry/registry.go
@@ -0,0 +1,73 @@
+package registry
+
+import "sync"
+
+// Registry is a generic data structure that maps strings to values of any type.
+type Registry[T any] struct {
+	m  map[string]T
+	mu sync.RWMutex
+}
+
+// NewRegistry creates a new instance of the Registry type.
+// The Registry is a generic data structure that maps strings to values of any type.
+func NewRegistry[T any]() *Registry[T] {
+	return &Registry[T]{
+		m: map[string]T{},
+	}
+}
+
+func isNil(t any) bool {
+	return t == nil
+}
+
+// Register adds a new value to the registry.
+// it panics if the name is empty, the value is nil it, or if the name is already in use.
+func (r *Registry[T]) Register(name string, t T) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	if len(name) == 0 {
+		panic("Registering with empty name")
+	}
+	if isNil(t) {
+		panic("Registering nil value")
+	}
+
+	if _, dup := r.m[name]; dup {
+		panic("Duplicated registration")
+	}
+
+	r.m[name] = t
+}
+
+// Lookup returns the value associated with the given name.
+// The second return value indicates whether the name was found in the registry.
+func (r *Registry[T]) Lookup(name string) (T, bool) {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+	t, ok := r.m[name]
+	return t, ok
+}
+
+// Unregister removes the value associated with the given name.
+func (r *Registry[T]) Unregister(name string) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	delete(r.m, name)
+}
+
+// List returns a list of all the names in the registry.
+func (r *Registry[T]) List() []string {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+	var list []string
+	for k := range r.m {
+		list = append(list, k)
+	}
+	return list
+}
+
+func (r *Registry[T]) Len() int {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+	return len(r.m)
+}
---
+++ registry/registry_test.go
@@ -0,0 +1,71 @@
+package registry_test
+
+import (
+	"slices"
+	"testing"
+
+	reg "github.com/workoak/wogo/registry"
+)
+
+type item interface {
+	MyName() string
+}
+type itemImpl struct {
+	name string
+}
+
+func (i *itemImpl) MyName() string {
+	return i.name
+}
+func TestRegistery(t *testing.T) {
+
+	r := reg.NewRegistry[string]()
+	r.Register("a", "aItem")
+	r.Register("b", "bItem")
+	r.Register("c", "cItem")
+	if len(r.List()) != 3 {
+		t.Errorf("Expected 3, got %d", len(r.List()))
+	}
+	if _, ok := r.Lookup("a"); !ok {
+		t.Errorf("Expected to find a")
+	}
+	if _, ok := r.Lookup("d"); ok {
+		t.Errorf("Expected not to find d")
+	}
+
+	names := r.List()
+	if len(names) != r.Len() || !slices.Contains(names, "a") || !slices.Contains(names, "b") || !slices.Contains(names, "c") {
+		t.Errorf("Expected to find a, b, and c")
+	}
+	r.Unregister("a")
+	if len(r.List()) != 2 {
+		t.Errorf("Expected 2, got %d", len(r.List()))
+	}
+
+}
+
+func TestRegisterNilPanic(t *testing.T) {
+
+	r2 := reg.NewRegistry[item]()
+	r2.Register("a", &itemImpl{name: "a"})
+	r2.Register("b", &itemImpl{name: "b"})
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("The code did not panic")
+		}
+	}()
+	r2.Register("c", nil)
+}
+
+func TestRegisterDupPanic(t *testing.T) {
+
+	r2 := reg.NewRegistry[item]()
+	r2.Register("a", &itemImpl{name: "a"})
+	r2.Register("b", &itemImpl{name: "b"})
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("The code did not panic")
+		}
+	}()
+	r2.Register("b", &itemImpl{name: "b"})
+}
